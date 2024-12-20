fd787b4c061bac8d55761e8fa7d6251b961ad348
ArnoBidet <arno.bidet479@gmail.com>
2024-12-20 17:15:19 +0100

    Add Flask API with RabbitMQ and Redis integration; update Dockerfiles and README

--- application/README.md
+++ application/README.md
@@ -7,3 +7,34 @@ Puisqu'on doit faire une calculatrice doit être réalisé, il faut en réaliser
 ![alt text](image.png)
 
 À gauche la vraie, à droite la reproduction.
+
+## API
+
+Nous avons décidé d'utiliser [flask-restful](https://flask-restful.readthedocs.io/en/latest/) afin de réaliser notre API
+
+### Packets à installer
+
+```python
+pip install flask-restful
+pip install redis
+```
+
+### Test par curl de l'API
+
+
+`Get`
+```
+curl -X GET 0.0.0.0:5000/api/v1/result?id=[REQUEST_ID]"
+```
+
+`Post`
+```
+curl -X POST 0.0.0.0:5000/api/v1/process -H "Content-Type: application/json" --data '{"calculation":"[CALCULATION]"}'
+```
+
+## Création d'environnement virtuel sous python - UBuntu
+
+```bash
+virtualenv venv 
+source venv/bin/activate
+```
---
+++ application/backend/app.py
@@ -0,0 +1,74 @@
+import uuid
+from flask import Flask, request, jsonify, abort
+import redis
+import pika
+import os
+
+# RABBITMQ
+def callback(ch, method, properties, body):
+    r.set(self.id_message, eval(body))
+    print(f" [x] Received {body}")
+
+# RabbitMQ connection details
+RABBITMQ_HOST = os.getenv('RABBITMQ_HOST', 'rabbitMQ')
+QUEUE_NAME = 'calculation'
+
+# Establish connection to RabbitMQ
+def get_rabbitmq_connection():
+    connection = pika.BlockingConnection(pika.ConnectionParameters(host=RABBITMQ_HOST))
+    properties = pika.BasicProperties(app_id='example-publisher', content_type='application/json', message_id='messageid')
+    return connection
+
+# REDIS
+r = redis.Redis(host='redis', port=6379, db=0, decode_responses=True)
+app = Flask(__name__)
+
+# API
+
+@app.route('/api/v1/result', methods=['GET'])
+def result():
+    id_param = request.args.get("id")
+    if id_param is None:
+        abort(400, description="id parameter is required")
+    value = r.get(id_param)
+    if value is None:
+        abort(404, description="result not found")
+    return {'result': value}
+
+
+@app.route('/api/v1/process', methods=['POST'])
+def process():
+    try:
+        print(f" [x] Received ")
+        # Get data from request
+        data = request.json
+        print(f" [x] Received {data}")
+        calculation = data.get('calculation', '')
+        print(f" [x] Received {calculation}")
+        # Check if calculation is provided
+        if not calculation:
+            return jsonify({'error': 'Message is required'}), 400
+        # Establish connection to RabbitMQ
+        connection = get_rabbitmq_connection()
+        channel = connection.channel()
+
+        # Declare queue
+        channel.queue_declare(queue=QUEUE_NAME, durable=True)
+        # Generate unique message id
+        message_id = str(uuid.uuid4())
+        full_message = {'id': message_id, 'message': calculation}
+
+        # Publish message
+        channel.basic_publish(exchange='',
+                              routing_key=QUEUE_NAME,
+                              body=full_message)
+        print(f" [x] Sent {full_message}")
+        # Close connection
+        connection.close()
+        # Return message id
+        return {'id': message_id}
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5000)
