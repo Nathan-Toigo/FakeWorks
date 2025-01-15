8e3edf577f39e3d2d4ca79094b388fcdfea551e1
nathan <nathan.toigo@laposte.net>
2025-01-15 18:07:01 +0100

    Add Angular Material support and refactor request list component

--- application/frontend/src/app/features/requestList/requestList.component.ts
+++ application/frontend/src/app/features/requestList/requestList.component.ts
@@ -1,38 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, inject, QueryList } from '@angular/core';
 import { MatTableModule } from '@angular/material/table';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIcon } from '@angular/material/icon';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { QueryRequest, QueryStatus } from './Request';
 
-export interface PeriodicElement {
-  name: string;
-  position: number;
-  weight: number;
-  symbol: string;
-}
-
-const ELEMENT_DATA: PeriodicElement[] = [
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
+const ELEMENT_DATA: QueryRequest[] = [
+  new QueryRequest("1+3"),
+  new QueryRequest("1+3"),
+  new QueryRequest("1+4"),
+  new QueryRequest("1+3"),
+  new QueryRequest("3+3"),
+  new QueryRequest("1+3"),
+  new QueryRequest("1+3"),
+  new QueryRequest("1+3"),
+  new QueryRequest("1+3"),
+  new QueryRequest("1+3"),
+  new QueryRequest("1+3")
 ];
 
-/**
- * @title Basic use of `<table mat-table>`
- */
 @Component({
   selector: 'table-basic',
   standalone: true,
-  styleUrl: 'requestList.component.scss',
+  styleUrls: ['requestList.component.scss'],
   templateUrl: 'requestList.component.html',
-  imports: [MatTableModule],
-})
+  imports: [MatTableModule, MatButtonModule, MatIcon],
+})  
 
 export class RequestListComponent {
-  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
+  
+  private _snackBar = inject(MatSnackBar);
+  
+  displayedColumns: string[] = ['calcul','result', 'status', 'UUID', 'requestButton'];
   dataSource = ELEMENT_DATA;
+
+  openSnackBar(message: string, action: string) {
+    this._snackBar.open(message + " copied in clipboard", action);
+  }
+
+  requestResult(request: string) {
+    this._snackBar.open(request + " has been sent", "OK");
+  }
 }
