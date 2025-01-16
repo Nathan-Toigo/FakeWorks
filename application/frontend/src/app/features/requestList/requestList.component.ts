580d8c953cb29d23119892a60db47cf602b2d422
Arno Bidet <arno.bidet479@gmail.com>
2025-01-16 01:39:19 +0100

    Enhance request list component with status-based result display and improved styling for empty rows

--- application/frontend/src/app/features/requestList/requestList.component.ts
+++ application/frontend/src/app/features/requestList/requestList.component.ts
@@ -13,31 +13,29 @@ import { ListService } from '../../shared/ListService';
   styleUrls: ['requestList.component.scss'],
   templateUrl: 'requestList.component.html',
   imports: [MatTableModule, MatButtonModule, MatIcon],
-})  
+})
 
 
 export class RequestListComponent {
-  
+
   private _snackBar = inject(MatSnackBar);
   displayedColumns: string[] = ['calcul','result', 'status', 'action'];
   dataSource : QueryRequest[] = [];
 
   constructor(private getResultService: GetResultService, private listService: ListService) {
     this.listService.list$.subscribe(updatedList => {
-      this.dataSource = updatedList.slice();
+      this.dataSource = updatedList.slice().sort((a, _) => a.status === QueryStatus.DONE ? 1 : -1);
     });
-  } 
-  
-  
+  }
 
-  openSnackBar(message: string, action: string) {
-    this._snackBar.open(message + " copied in clipboard", action);
+  openSnackBar(message: string) {
+    this._snackBar.open(message + " copied in clipboard", "", {duration : 2000});
   }
 
   requestResult(request: string) {
-    this._snackBar.open(request + " has been sent", "OK");
+    this._snackBar.open(request + " has been sent", "", {duration : 2000});
 
-    
+
     this.getResultService.getResults(request).subscribe({
       next: (data) => {
         this.listService.updateRequest(request, QueryStatus.DONE, data.result);
