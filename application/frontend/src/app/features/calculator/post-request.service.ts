8f272057ec4ab7d29f69203115b12d142bd72842
ArnoBidet <arno.bidet479@gmail.com>
2025-02-06 18:17:39 +0100

    Refactor frontend build process and environment configuration for production

--- application/frontend/src/app/features/calculator/post-request.service.ts
+++ application/frontend/src/app/features/calculator/post-request.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { PostRequest } from './post-request-interface';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { environment } from '../../../environment';
+import { environment } from '../../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
